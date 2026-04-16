f387b885f2b51e5719cc24c8e5e01550326f1a5e
ayushsinha12 <ayushsinha2445@gmail.com>
2026-04-16 14:42:37 -0400

    Complete a6 MongoDB Atlas integration

--- kambaz/enrollments/dao.js
+++ kambaz/enrollments/dao.js
@@ -1,39 +1,37 @@
-import { v4 as uuidv4 } from "uuid";
+import model from "./model.js";
 
 export default function EnrollmentsDao(db) {
-  function findEnrollmentsForUser(userId) {
-    const { enrollments } = db;
-    return enrollments.filter((enrollment) => enrollment.user === userId);
+  async function findCoursesForUser(userId) {
+    const enrollments = await model.find({ user: userId }).populate("course");
+    return enrollments.map((enrollment) => enrollment.course);
+  }
+
+  async function findUsersForCourse(courseId) {
+    const enrollments = await model.find({ course: courseId }).populate("user");
+    return enrollments.map((enrollment) => enrollment.user);
   }
 
   function enrollUserInCourse(userId, courseId) {
-    const { enrollments } = db;
-    const existingEnrollment = enrollments.find(
-      (enrollment) =>
-        enrollment.user === userId && enrollment.course === courseId
-    );
-    if (existingEnrollment) {
-      return existingEnrollment;
-    }
-    const newEnrollment = {
-      _id: uuidv4(),
+    return model.create({
       user: userId,
       course: courseId,
-    };
-    db.enrollments = [...db.enrollments, newEnrollment];
-    return newEnrollment;
+      _id: `${userId}-${courseId}`,
+    });
   }
 
-  function unenrollUserFromCourse(userId, courseId) {
-    db.enrollments = db.enrollments.filter(
-      (enrollment) =>
-        !(enrollment.user === userId && enrollment.course === courseId)
-    );
+  function unenrollUserFromCourse(user, course) {
+    return model.deleteOne({ user, course });
+  }
+
+  function unenrollAllUsersFromCourse(courseId) {
+    return model.deleteMany({ course: courseId });
   }
 
   return {
-    findEnrollmentsForUser,
+    findCoursesForUser,
+    findUsersForCourse,
     enrollUserInCourse,
     unenrollUserFromCourse,
+    unenrollAllUsersFromCourse,
   };
 }
